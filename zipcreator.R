146a4b285a32e2c9a6f780fe263cad744b0c8dcf
ChrisJones687 <cjones1688@gmail.com>
2018-04-17 16:14:34 -0400

    fixed zip creator

--- zipcreator.R
+++ zipcreator.R
@@ -5,6 +5,6 @@ zipcreator <- function(dataList, path) {
     writeRaster(dataList[[ix]], file = paste("output/host",p,".tif",sep = ""), overwrite=TRUE)
   }
   files2zip <- dir("output", full.names = TRUE)
-  zip(zipfile = "C:/Users/Chris/Desktop/testZip2.zip", files = files2zip)
+  zip(zipfile = path, files = files2zip)
   do.call(file.remove, list(list.files("output", full.names = TRUE)))
 }
