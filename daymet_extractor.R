fcdbe7cdfb9cedf430c42c62e8f7e10660ba9ab5
ChrisJones687 <cjones1688@gmail.com>
2018-05-22 06:12:25 -0400

    update

--- daymet_extractor.R
+++ daymet_extractor.R
@@ -3,23 +3,24 @@ library(rgdal)
 library(raster)
 library(ncdf4)
 library(sp)
+library(googledrive)
 
 daymet_setup <- function(directory, output_directory, start, end, timestep, states_of_interest= c('California'), variables = c("prcp")){
   
 }
 
 ## Setup start and stop years
-start <-  2012
+start <-  2014
 end <- 2017
 time_range <- seq(start, end, 1)
 ## set up directory with files and create a new directory based on location of interest
-setwd("G:/DaymetUS2")
 directory <- "G:/DaymetUS"
-output_directory <- dir.create("G:/DaymetUS/California")
+#output_directory <- dir.create("G:/DaymetUS/California")
 
 ## reference shapefile used to clip, project, and resample 
 states <- readOGR("C:/Users/Chris/Desktop/California/us_states_lccproj.shp") # link to your local copy
-reference_area <- states[states@data$STATE_NAME %in% c('Delaware','District of Columbia','New Jersey','Virginia', 'Pennsylvania', 'Maryland'),]
+#reference_area <- states[states@data$STATE_NAME %in% c('Delaware','District of Columbia','New Jersey','Virginia', 'Pennsylvania', 'Maryland'),]
+reference_area <- states[states@data$STATE_NAME %in% states_of_interest,]
 rm(states)
 
 ## list of files
@@ -32,14 +33,15 @@ precip_files <- precip_files[dates %in% time_range]
 tmin_files <- tmin_files[dates %in% time_range]
 tmax_files <- tmax_files[dates %in% time_range]
 
-
-setwd("G:/DaymetUS/SLF_area")
+dir.create(output_directory)
+setwd(output_directory)
 
 for (i in 1:length(precip_files)) {
   precip <- stack(precip_files[[i]], varname = "prcp")
   precip <- crop(precip, reference_area)
   precip <- mask(precip, reference_area)
-  writeRaster(x=precip, filename = paste("daymet_v3_prcp_",time_range[i],"_slfarea.tif", sep = ""), overwrite=TRUE, format = 'GTiff')
+  
+  #writeRaster(x=precip, filename = paste("daymet_v3_prcp_",time_range[i],"_slfarea.tif", sep = ""), overwrite=TRUE, format = 'GTiff')
   print(i)
 }
 
@@ -61,18 +63,3 @@ for (i in 2:length(tmax_files)){
   writeRaster(x=tavg, filename = paste("daymet_v3_tavg_",time_range[i],"_slfarea.tif", sep = ""), overwrite=TRUE, format = 'GTiff')
   print(i)
 }
-
-
-## Using ncdf4
-p_list <- lapply(precip_files, nc_open)
-tmax_list <- lapply(tmax_files, nc_open)
-tmin_list <- lapply(tmin_files, nc_open)
-
-## pull out lat, lon, and time
-lat <- ncvar_get(p_list[[1]], varid = "lat")
-lon <- ncvar_get(p_list[[1]], varid = "lon")
-time <- ncvar_get(p_list[[1]], varid = "time")
-
-pvar <- ncvar_get(p_list[[1]], varid = "prcp")
-tmax_var <- ncvar_get(tmax_list[[1]], varid = "tmax")
-tmin_var <- ncvar_get(tmin_list[[1]], varid = "tmin")
