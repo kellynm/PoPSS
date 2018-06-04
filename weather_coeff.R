d30dc1967f056f83a1b8cb3de0921a72515357ce
ChrisJones687 <cjones1688@gmail.com>
2018-06-04 12:59:52 -0400

    update weather

--- weather_coeff.R
+++ weather_coeff.R
@@ -22,43 +22,46 @@ weather_coeff <- function(directory, output_directory, start, end, timestep, sta
   reference_area <- states[states@data$STATE_NAME %in% states_of_interest,]
   rm(states)
   
+  ## Create raster stacks for the area of interest and years of interest from Daymet data
   prec <- stack()
+  tmin_s <- stack()
+  tmax_s <- stack()
+  tavg_s <- stack()
   for (i in 1:length(precip_files)) {
+    ## Precipitation 
     precip <- stack(precip_files[[i]], varname = "prcp")
     precip <- crop(precip, reference_area)
     precip <- mask(precip, reference_area)
     if (i>1 && compareCRS(precip,prec) == FALSE) { precip@crs <- crs(prec) }
     prec <- stack(prec, precip)
-    print(i)
     rm(precip)
+    
+    ## Temperature
+    tmin <- stack(tmin_files[[i]], varname = "tmin")
+    tmin <- crop(tmin, reference_area)
+    tmin <- mask(tmin, reference_area)
+    if (i>1 && compareCRS(tmin,tmin_s) == FALSE) { tmin@crs <- crs(tmin_s) }
+    tmax <- stack(tmax_files[[i]], varname = "tmax")
+    tmax <- crop(tmax, reference_area)
+    tmax <- mask(tmax, reference_area)
+    if (i>1 && compareCRS(tmax,tmax_s) == FALSE) { tmax@crs <- crs(tmax_s) }
+    tavg <- tmax
+    for (j in 1:nlayers(tmax)){
+      tavg[[j]] <- overlay(tmax[[j]], tmin[[j]], fun = function(r1, r2){return((r1+r2)/2)})
+      print(j)
+    }
+    tmin_s <- stack(tmin, tmin_s)
+    tmax_s <- stack(tmax, tmax_s)
+    tavg_s <- stack(tavg, tavg_s)
+    
+    print(i)
   }
   
   ## create directory for writing files
   dir.create(output_directory)
   
   ## Write outputs as raster format to output directory
-  writeRaster(x=precip, filename = paste(output_directory, "/prcp_coeff_", start, "_", end, "_", pest, ".tif", sep = ""), overwrite=TRUE, format = 'GTiff')
+  writeRaster(x=prec, filename = paste(output_directory, "/prcp_coeff_", start, "_", end, "_", pest, ".tif", sep = ""), overwrite=TRUE, format = 'GTiff')
   writeRaster(x=temp, filename = paste(output_directory, "/temp_coeff_", start, "_", end, "_", pest, ".tif", sep = ""), overwrite=TRUE, format = 'GTiff')
   
-}
-
-
-
-## Create Tavg
-for (i in 2:length(tmax_files)){
-  tmin <- stack(tmin_files[[i]], varname = "tmin")
-  tmin <- crop(tmin, reference_area)
-  tmin <- mask(tmin, reference_area)
-  tmax <- stack(tmax_files[[i]], varname = "tmax")
-  tmax <- crop(tmax, reference_area)
-  tmax <- mask(tmax, reference_area)
-  tavg <- tmax
-  for (j in 1:nlayers(tmax)){
-    tavg[[j]] <- overlay(tmax[[j]], tmin[[j]], fun = function(r1, r2){return((r1+r2)/2)})
-    print(j)
-  }
-  writeRaster(x=tmax, filename = paste("daymet_v3_tmax_",time_range[i],"_slfarea.tif", sep = ""), overwrite=TRUE, format = 'GTiff')
-  writeRaster(x=tmin, filename = paste("daymet_v3_tmin_",time_range[i],"_slfarea.tif", sep = ""), overwrite=TRUE, format = 'GTiff')
-  writeRaster(x=tavg, filename = paste("daymet_v3_tavg_",time_range[i],"_slfarea.tif", sep = ""), overwrite=TRUE, format = 'GTiff')
-  print(i)
 }
